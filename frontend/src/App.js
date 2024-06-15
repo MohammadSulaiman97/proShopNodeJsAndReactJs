7977c76451212f987bfc4ddfa5d157fc7dc09226
Mohammad Sulaiman <mohammad.sulaiman@vianoes.com>
2024-06-15 19:14:39 +0300

    add routes between homeScreen and productScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
+import {Container} from 'react-bootstrap'
 import Header from "./compoents/Header";
+import Footer from "./compoents/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from './screens/ProductScreen';
 
 
 function App() {
   return (
-    <>
-     <Header />
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={ <HomeScreen /> } exact />
+            <Route path='/product/:id' element={ <ProductScreen /> } />
+          </Routes>
+        </Container> 
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
